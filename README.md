4c6b5dc0fc1ae2893f264e0aa2ab153c23d91c10
Josh Vincent <josh.vincent123+bitbucket@gmail.com>
2021-04-12 11:45:14 +1000

    add readme and lambda code

---
+++ README.md
@@ -0,0 +1,44 @@
+# Lambda stripe assign to custom attribute confirm signup trigger
+
+Lambda function after user confirms account adds user to stripe and then updated custom cognito field.
+
+### Dependancies
+
+```
+npm i ftx-rest-api
+```
+
+### Prerequisites
+
+Create new secrets manager
+Create you api in FTX and copy the values into FTX_API_SECRET: 'API SECRET HERE ' & FTX_API_KEY: 'API VALUE HERE'
+
+Make sure to update your lambda policy to include
+
+```
+{
+  "Effect": "Allow",
+  "Action": [
+    "secretsmanager:GetSecretValue"
+  ],
+  "Resource": {
+    "Fn::Sub": [
+      "arn:aws:secretsmanager:${region}:${account}:secret:FTX_MAIN-0RPnWV",
+      {
+        "region": {
+          "Ref": "AWS::Region"
+        },
+        "account": {
+          "Ref": "AWS::AccountId"
+        }
+      }
+    ]
+  }
+}
+```
+
+## Setup Trigger
+Add Trigger
+EventBridge(CloudWatch Events)
+Create a new rule
+Schedule expression cron(50 * * * ? *)
---
+++ index.js
@@ -0,0 +1,59 @@
+const AWS = require('aws-sdk')
+const FTXRest = require('ftx-api-rest')
+//cron(50 * * * ? *) run every hour on at 50mins
+exports.handler = async (event, callback) =>
+{
+  const secrets = await getSecret('FTX-ID-SECRETS-MANAGER')
+  const { FTX_API_SECRET, FTX_API_KEY } = secrets
+  const ftx = new FTXRest({ key: FTX_API_KEY, secret: FTX_API_SECRET, subaccount: subAccount })
+
+  let currency = 'USD'
+  let subAccount = '' // or sub account name here
+
+  const current = await getCurrentBalances(ftx).catch(err => console.log(err))
+  console.log("Current balances:", current);
+
+  const currrencyObject = current.result.find(result => result.coin === currency);
+  console.log("currrencyObject", currrencyObject)
+
+  const lending = await lendAll(ftx, currrencyObject).catch(err => console.log(err))
+  console.log("lending result:", lending)
+  return lending
+};
+
+async function getSecret(secretId)
+{
+  const secretsManager = new AWS.SecretsManager()
+  const secret = await secretsManager.getSecretValue({ SecretId: secretId }).promise()
+  if (!secret)
+  {
+    throw new Error('Secret not found')
+  }
+  let secretString = JSON.parse(secret.SecretString);
+  return secretString
+}
+
+async function getCurrentBalances(ftx)
+{
+  const getBalancesResult = await ftx.request({
+    method: 'GET',
+    path: '/wallet/balances',
+  })
+  return getBalancesResult
+}
+
+async function lendAll(ftx, currencyObject)
+{
+  const { coin, total } = currencyObject
+  const offersResult = await ftx.request({
+    method: 'POST',
+    path: '/spot_margin/offers',
+    data: {
+      coin: coin,
+      size: total,
+      rate: 0.000001,
+    },
+  })
+
+  return offersResult
+}
